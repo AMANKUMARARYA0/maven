0a356d8fab03b773862a4130449c91be74f71ac1
AMAN <amanarya.me1@gmail.com>
2026-03-13 22:39:03 +0530

    eatf

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,48 +1,13 @@
-pipeline {
-    agent any
+package demo;
 
-    tools {
-        maven 'Maven'
-    }
-
-    triggers {
-        githubPush()
-    }
-
-    stages {
-
-        stage('Clone Repository') {
-            steps {
-                git branch: 'main', url: 'https://github.com/AMANKUMARARYA0/maven.git'
-            }
-        }
-
-        stage('Compile') {
-            steps {
-                sh 'mvn clean compile'
-            }
-        }
-
-        stage('Run Tests') {
-            steps {
-                sh 'mvn test'
-            }
-        }
-
-        stage('Package') {
-            steps {
-                sh 'mvn package'
-            }
-        }
-
-    }
-
-    post {
-        success {
-            echo 'Build and Tests Passed ✅'
-        }
-        failure {
-            echo 'Build Failed ❌'
-        }
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Aman! from CI CD task package hi from demo by jenkins" );
     }
 }
